fb05e388befbf0645bfecf244b3f11db14c9c71f
Victor Nguyen <victormdnguyen88@gmail.com>
2018-04-26 17:44:44 +0000

    add error status

--- op_pint.c
+++ op_pint.c
@@ -9,6 +9,9 @@
 
 void op_pint(stack_t **stack, unsigned int line_number)
 {
-
-
+	if (*stack == NULL)
+	{
+		fprintf(stdout, "L<%d>: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 }
