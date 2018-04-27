9c216cf7655a99c6add34f1d0d2ec666c0e331fc
arturadamian <arturadamian@gmail.com>
2018-04-27 05:14:38 +0000

    fix the negative

--- _isdigit.c
+++ _isdigit.c
@@ -11,12 +11,12 @@ int _isdigit(const char *tok_data)
 	int i = 0;
 
 	if (!tok_data)
-		exit(EXIT_FAILURE);
+		exit(0);
 	if (tok_data[i] == '-')
 		i++;
 	while (tok_data[i] != '\0')
 	{
-		if (isdigit(tok_data[i]) == 0)
+		if (!isdigit(tok_data[i]))
 		{
 			exit(EXIT_FAILURE);
 		}
