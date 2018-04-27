847ce527d9c6fe7a1af4b548aa7885eb9b2beb1a
arturadamian <arturadamian@gmail.com>
2018-04-27 04:47:38 +0000

    sub

--- op_sub.c
+++ op_sub.c
@@ -11,7 +11,7 @@ void op_sub(stack_t **stack, unsigned int line_number)
 {
 	int summ;
 
-	if ((*stack) == NULL || (*stack)->next == NULL)
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
 	{
 		printf("L%d: can't sub, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
