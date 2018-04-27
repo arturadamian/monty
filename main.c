5467b3bff2a3f947d096b59c409daa2a7e3782c2
Victor Nguyen <victormdnguyen88@gmail.com>
2018-04-27 00:27:24 +0000

    rearrange logic

--- main.c
+++ main.c
@@ -9,14 +9,11 @@
 
 int main(int argc, char *argv[])
 {
-	char *buffer = NULL;
-	size_t buffsize = 0;
 	FILE *fd;
+	char *buffer = NULL, *token = NULL;
+	size_t buffsize = 0, *stack = NULL;
 	unsigned int line_number = 0;
-	stack_t *stack = NULL;
 	instruction_t *f = NULL;
-	char *token = NULL;
-
 
 	if (argc != 2)
 	{
@@ -25,13 +22,12 @@ int main(int argc, char *argv[])
 	}
 
 	fd = fopen(argv[1], "r"); /* opens an reads */
-
-
 	if (fd == NULL)
 	{
 		printf("Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
+
 	while (getline(&buffer, &buffsize, fd) != -1)
 	{
 		line_number++;
