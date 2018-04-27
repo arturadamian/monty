1b0fb4b96411234a827e044c820eb8816dc83fe4
arturadamian <arturadamian@gmail.com>
2018-04-27 05:02:53 +0000

    add condition for mod / 0

--- op_mod.c
+++ op_mod.c
@@ -16,6 +16,12 @@ void op_mod(stack_t **stack, unsigned int line_number)
 		printf("L%d: can't mod, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
+
+	if ((*stack)->n == 0)
+	{
+		printf("L%d: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 	modd = (*stack)->next->n % (*stack)->n;
 	op_pop(stack, line_number);
 	(*stack)->n = modd;
